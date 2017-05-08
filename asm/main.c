ac3a68eee7455d05829e03fda9f253953bdab1cd
NickS <NickS@MacBook-Pro.local>
2017-05-08 18:57:18 +0300

    asm

--- asm/main.c
+++ asm/main.c
@@ -1,20 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "asm.h"
+#include "../op.h"
 
-
-
-void    validate(char *line, FILE *file, t_data *data)
+int check_comment(char *line)
 {
+	printf("check comment\n");
+	printf("%s\n", line);
+	return (1);
+}
+
+
+int check_name(char *line)
+{
+	printf("check name\n");
+	printf("%s\n", line);
+	return (1);
+}
+
+
+int   validate(int fd, t_data *data)
+{
+	char *line;
+
+	while (get_next_line(fd, &line))
+	{
+		if (ft_strstr(line, NAME_CMD_STRING))
+			if (!check_name(line))
+				return (0);
+		if (ft_strstr(line, COMMENT_CMD_STRING))
+			if (!check_comment(line))
+				return (0);
+
+	}
+	return (1);
 
 }
 
 int main(int argc, char **argv)
 {
 	int fd;
-	char *line;
 	t_data *data;
-	int i;
 
 	data = (t_data*)malloc(sizeof(*data));
 	if (argc != 2)
@@ -27,14 +53,8 @@ int main(int argc, char **argv)
 		printf("Cannot read \"{%s}\" file", argv[1]);
 		return 0;
 	}
-	while (get_next_line(fd, &line))
-	{
-		int len = ft_strlen(line);
-		data->file = (char *)malloc(sizeof(char) * (len + 1));
-		data->file = ft_strjoin(line, data->file);
-		data->file[len + 1] = ';';
-	}
-	printf("%s\n", data->file);
+	if (!validate(fd, data))
+		return (0);
 	return (0);
 
 }
