7c36ef31fa7de6ba9b84cd80a594321380003e69
NickS <NickS@MacBook-Pro.local>
2017-05-12 11:31:30 +0300

    comment and name

--- asm/asm.h
+++ asm/asm.h
@@ -1,7 +1,13 @@
 #ifndef ASM_H
 #define ASM_H
 #include "../libft/libft.h"
-#include "../libft/ft_printf/header.h"
+//#include "../libft/ft_printf/header.h"
+
+#define NAME_ERROR "Champion name to long (Max length 128 bytes)\n"
+#define COMMENT_ERROR "Champion comment too long (Max length 2048)\n"
+#define LEXICAL_ERROR "Lexical at [%d : %d]\n"
+
+int num_line = 0;
 
 typedef struct s_data
 {
--- asm/main.c
+++ asm/main.c
@@ -3,18 +3,72 @@
 #include "asm.h"
 #include "../op.h"
 
-int check_comment(char *line)
+
+int check_comment(int fd, char *line)
 {
-	printf("check comment\n");
-	printf("%s\n", line);
+	printf("%d\n", num_line);
+	static int quotes = 0;
+	static int read_byte = 0;
+	int i;
+
+	i = 0;
+	while (line[i])
+	{
+		if (line[i] == '\"')
+			quotes++;
+		i++;
+		read_byte++;
+	}
+	if (read_byte > COMMENT_LENGTH)
+	{
+		printf(COMMENT_ERROR);
+		return (0);
+	}
+	if (quotes == 2 && read_byte < COMMENT_LENGTH) {
+		printf("comment valid\n");
+		return (1);
+	}
+	else
+	{
+		free(line);
+		get_next_line(fd, &line);
+		num_line++;
+		check_comment(fd, line);
+	}
 	return (1);
 }
 
 
-int check_name(char *line)
+int check_name(int fd, char *line)
 {
-	printf("check name\n");
-	printf("%s\n", line);
+	static int quotes = 0;
+	static int read_byte = 0;
+	int i;
+
+	i = 0;
+	while (line[i])
+	{
+		if (line[i] == '\"')
+			quotes++;
+		i++;
+		read_byte++;
+	}
+	if (read_byte > PROG_NAME_LENGTH)
+	{
+		printf(NAME_ERROR);
+		return (0);
+	}
+	if (quotes == 2 && read_byte < PROG_NAME_LENGTH) {
+		printf("name valid\n");
+		return (1);
+	}
+	else
+	{
+		free(line);
+		get_next_line(fd, &line);
+		num_line++;
+		check_name(fd, line);
+	}
 	return (1);
 }
 
@@ -25,19 +79,26 @@ int   validate(int fd, t_data *data)
 
 	while (get_next_line(fd, &line))
 	{
+		num_line++;
+		if (line[0] == COMMENT_CHAR)
+			continue ;
 		if (ft_strstr(line, NAME_CMD_STRING))
-			if (!check_name(line))
+			if (!check_name(fd, line))
 				return (0);
 		if (ft_strstr(line, COMMENT_CMD_STRING))
-			if (!check_comment(line))
+			if (!check_comment(fd, line))
 				return (0);
+		if (line[0] == '<')
+		{
+			printf(LEXICAL_ERROR, num_line, 1);
+		}
 
 	}
 	return (1);
 
 }
 
-int main(int argc, char **argv)
+int main(int argc , char **argv)
 {
 	int fd;
 	t_data *data;
