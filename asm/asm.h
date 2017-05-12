dcba946914158d76f8fda298cb2d36910ff34eb3
Nick Simonov <nsimonov@e1r2p15.unit.ua>
2017-05-12 20:32:07 +0300

    asm unit

--- asm/asm.h
+++ asm/asm.h
@@ -5,13 +5,29 @@
 
 #define NAME_ERROR "Champion name to long (Max length 128 bytes)\n"
 #define COMMENT_ERROR "Champion comment too long (Max length 2048)\n"
-#define LEXICAL_ERROR "Lexical at [%d : %d]\n"
+#define LEXICAL_ERROR "Lexical error at [%d : %d]\n"
 
-int num_line = 0;
+int g_num_line = 0;
+int g_start_mem_arr_size = 5;
+int g_new_mem_arr_size = 1;
+int g_i = 0;
+
+typedef struct       lst
+{
+	char         *lable;
+	char       *command;
+	int          arg_nb;
+	int         op_code;
+	struct lst    *next;
+}                 m_lst;
 
 typedef struct s_data
 {
-	char *file;
+	int     name;
+	int     comment;
+	char    **array;
+	m_lst *lst;
 }              t_data;
 
 #endif
+
--- asm/main.c
+++ asm/main.c
@@ -4,96 +4,151 @@
 #include "../op.h"
 
 
-int check_comment(int fd, char *line)
+int check_byte(int read_byte, char *name)
 {
-	printf("%d\n", num_line);
-	static int quotes = 0;
-	static int read_byte = 0;
-	int i;
-
-	i = 0;
-	while (line[i])
+	if (!ft_strcmp(name, COMMENT_CMD_STRING))
 	{
-		if (line[i] == '\"')
-			quotes++;
-		i++;
-		read_byte++;
+		if (read_byte > COMMENT_LENGTH)
+		{
+			printf(COMMENT_ERROR);
+			return (0);
+		}
 	}
-	if (read_byte > COMMENT_LENGTH)
-	{
-		printf(COMMENT_ERROR);
-		return (0);
-	}
-	if (quotes == 2 && read_byte < COMMENT_LENGTH) {
-		printf("comment valid\n");
-		return (1);
-	}
-	else
-	{
-		free(line);
-		get_next_line(fd, &line);
-		num_line++;
-		check_comment(fd, line);
-	}
-	return (1);
-}
-
-
-int check_name(int fd, char *line)
-{
-	static int quotes = 0;
-	static int read_byte = 0;
-	int i;
-
-	i = 0;
-	while (line[i])
-	{
-		if (line[i] == '\"')
-			quotes++;
-		i++;
-		read_byte++;
-	}
-	if (read_byte > PROG_NAME_LENGTH)
+	else if (read_byte > PROG_NAME_LENGTH)
 	{
 		printf(NAME_ERROR);
 		return (0);
 	}
-	if (quotes == 2 && read_byte < PROG_NAME_LENGTH) {
-		printf("name valid\n");
+	return (1);
+}
+
+int check_comment(int fd, char *line, t_data *data)
+{
+	static int quotes = 0;
+	static int read_byte = 0;
+	int i;
+
+	i = 0;
+	while (line[i])
+	{
+		if (line[i] == '\"')
+			quotes++;
+		read_byte++;
+		i++;
+	}
+	if (!check_byte(read_byte, COMMENT_CMD_STRING))
+		return (0);
+	if (quotes == 2) {
+			data->comment = 1;
+			return (1);
+	}
+	else
+	{
+		free(line);
+		get_next_line(fd, &line);
+		g_num_line++;
+		check_comment(fd, line, data);
+	}
+	return (1);
+}
+
+int check_name(int fd, char *line, t_data *data)
+{
+	static int quotes = 0;
+	static int read_byte = 0;
+	int i;
+
+	i = 0;
+	while (line[i])
+	{
+		if (line[i] == '\"')
+			quotes++;
+		read_byte++;
+		i++;
+	}
+	if (!check_byte(read_byte, NAME_CMD_STRING))
+		return (0);
+	if (quotes == 2)
+	{
+		data->name = 1;
 		return (1);
 	}
 	else
 	{
 		free(line);
 		get_next_line(fd, &line);
-		num_line++;
-		check_name(fd, line);
+		g_num_line++;
+		check_name(fd, line, data);
 	}
 	return (1);
 }
 
+void copy_file(char *line, t_data *data)
+{
+	int index;
+	int j;
+	int new_mem;
+
+	new_mem = 0;
+	index = 0;
+	if (g_i > g_start_mem_arr_size)
+	{
+		new_mem = g_start_mem_arr_size + g_new_mem_arr_size;
+		g_new_mem_arr_size++;
+		data->array = (char**)realloc(data->array, sizeof(char *) * new_mem + 1);
+		data->array[new_mem] = 0;
+	}
+	data->array[g_i] = (char *)malloc(sizeof(char *) * ft_strlen(line) + 1);
+	j = 0;
+	while (line[index])
+	{
+		data->array[g_i][j] = line[index];
+		j++;
+		index++;
+	}
+	data->array[g_i][j] = '\0';
+	g_i++;
+}
+
+void    make_array(t_data *data)
+{
+	if ((data->array = (char **)malloc(sizeof(char *) * g_start_mem_arr_size + 1)) == NULL)
+		return ;
+	data->array[g_start_mem_arr_size] = 0;
+}
 
 int   validate(int fd, t_data *data)
 {
 	char *line;
 
+	make_array(data);
 	while (get_next_line(fd, &line))
 	{
-		num_line++;
+		g_num_line++;
+		if (line[0] == '\0')
+		{
+			g_num_line--;
+			free(line);
+			continue;
+		}
+		if (data->name && data->comment)
+		{
+			copy_file(line, data);
+		}
 		if (line[0] == COMMENT_CHAR)
-			continue ;
+		{
+			free(line);
+			continue;
+		}
 		if (ft_strstr(line, NAME_CMD_STRING))
-			if (!check_name(fd, line))
+			if (!check_name(fd, line, data))
 				return (0);
 		if (ft_strstr(line, COMMENT_CMD_STRING))
-			if (!check_comment(fd, line))
+			if (!check_comment(fd, line, data))
 				return (0);
-		if (line[0] == '<')
-		{
-			printf(LEXICAL_ERROR, num_line, 1);
-		}
-
+		free(line);
 	}
+	printf("end while start printing...\n");
 	return (1);
 
 }
@@ -104,6 +159,8 @@ int main(int argc , char **argv)
 	t_data *data;
 
 	data = (t_data*)malloc(sizeof(*data));
+	data->name = 0;
+	data->comment = 0;
 	if (argc != 2)
 	{
 		printf("usage: file with player {.s}");
@@ -116,6 +173,8 @@ int main(int argc , char **argv)
 	}
 	if (!validate(fd, data))
 		return (0);
+	printf("Gnum %d\n", g_num_line);
+	printf("%s", data->array[200]);
 	return (0);
 
 }
