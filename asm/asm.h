829faec99efa1c5d35b9f285aedf272edbdd8acd
NickS <NickS@MacBook-Pro.local>
2017-05-16 22:50:14 +0300

    14.05 23:00

--- asm/asm.h
+++ asm/asm.h
@@ -6,6 +6,7 @@
 #define NAME_ERROR "Champion name to long (Max length 128 bytes)\n"
 #define COMMENT_ERROR "Champion comment too long (Max length 2048)\n"
 #define LEXICAL_ERROR "Lexical error at [%d : %d]\n"
+#define PROG_NAME_ERROR "Usage: <sourcefile.s> other extension not supported\n"
 
 int g_num_line = 0;
 int g_start_mem_arr_size = 5;
@@ -16,15 +17,15 @@ typedef struct       lst
 {
 	char         *lable;
 	char       *command;
-	int          arg_nb;
 	int         op_code;
 	struct lst    *next;
 }                 m_lst;
 
 typedef struct s_data
 {
-	int     name;
-	int     comment;
+	int itfirst;
+	int name;
+	int comment;
 	char    **array;
 	m_lst *lst;
 
--- asm/main.c
+++ asm/main.c
@@ -22,13 +22,12 @@ int check_byte(int read_byte, char *name)
 	return (1);
 }
 
-int check_comment(int fd, char *line, t_data *data)
+int check_comment(int k, char *line, t_data *data)
 {
 	static int quotes = 0;
 	static int read_byte = 0;
-	int i;
+	int i = 0;
 
-	i = 0;
 	while (line[i])
 	{
 		if (line[i] == '\"')
@@ -38,21 +37,17 @@ int check_comment(int fd, char *line, t_data *data)
 	}
 	if (!check_byte(read_byte, COMMENT_CMD_STRING))
 		return (0);
-	if (quotes == 2) {
-			data->comment = 1;
-			return (1);
+	if (quotes == 2)
+	{
+		data->comment = 1;
+		return (1);
 	}
 	else
-	{
-		free(line);
-		get_next_line(fd, &line);
-		g_num_line++;
-		check_comment(fd, line, data);
-	}
+		check_comment(k + 1, data->array[k + 1], data);
 	return (1);
 }
 
-int check_name(int fd, char *line, t_data *data)
+int check_name(int k, char *line, t_data *data)
 {
 	static int quotes = 0;
 	static int read_byte = 0;
@@ -74,12 +69,7 @@ int check_name(int fd, char *line, t_data *data)
 		return (1);
 	}
 	else
-	{
-		free(line);
-		get_next_line(fd, &line);
-		g_num_line++;
-		check_name(fd, line, data);
-	}
+		check_name(k + 1, data->array[k + 1], data);
 	return (1);
 }
 
@@ -145,12 +135,25 @@ void    write_line(m_lst **lst, char *line)
 	}
 
 }
+
+int parse_line(char *line, t_data *data)
+{
+	int i;
+
+	i = 0;
+	if (is_lbl(line))
+		return (0);
+	if (is_cmd(line))
+		return (0);
+}
+
 void    parse_file(t_data *data)
 {
 	int i;
 	m_lst *head;
 
 	i = 0;
+	int j = 0;
 	while (data->array[i])
 	{
 		if (!ft_strcmp("", data->array[i]))
@@ -158,26 +161,56 @@ void    parse_file(t_data *data)
 			i++;
 			continue ;
 		}
-		write_line(&(data->lst), data->array[i]);
+		//write_line(&(data->lst), data->array[i]);
+		if (ft_strstr(data->array[i], NAME_CMD_STRING) && data->itfirst)
+			if (!check_name(i, data->array[i], data))
+				return ;
+		if (ft_strstr(data->array[i], COMMENT_CMD_STRING) && data->itfirst)
+			if (!check_comment(i, data->array[i], data))
+				return ;
+		if (!parse_line(data->array[i], data))
+			return ;
 		i++;
 	}
-	head = data->lst;
-	while (head != NULL)
-	{
-		printf("LST {%s}\n", head->command);
-		head = head->next;
-	}
+//	head = data->lst;
+//	while (head != NULL)
+//	{
+//		printf("LST {%s}\n", head->command);
+//		head = head->next;
+//	}
 	printf("before sellp\n");
 	sleep(222);
 
 }
-int   validate(int fd, t_data *data)
+
+int   check_prog_name(char *prog_name)
 {
+	int len;
+
+	len = ft_strlen(prog_name) - 1;
+	while (prog_name[len] && len > 0)
+	{
+		if (prog_name[len] == 's' && prog_name[len - 1] == '.') {
+			printf("valid prog\n");
+			return (1);
+		}
+		len--;
+	}
+	printf(PROG_NAME_ERROR);
+	return (0);
+
+}
+
+
+int   validate(int fd, t_data *data, char *prog_name)
+{
+	if (!check_prog_name(prog_name))
+		return (0);
 	make_array(data);
 	copy_file_to_array(data, fd);
 	parse_file(data);
-//	if (is_valid_file(data))
-//		return (1);
+	//if (is_valid_file(data))
+	//	return (1);
 //	return (0);
 //		if (ft_strstr(line, NAME_CMD_STRING))
 //			if (!check_name(fd, line, data))
@@ -196,12 +229,13 @@ int main(int argc , char **argv)
 	t_data *data;
 
 	data = (t_data*)malloc(sizeof(*data));
+	data->itfirst = 1;
 	data->name = 0;
 	data->comment = 0;
 	data->lst = NULL;
 	if (argc != 2)
 	{
-		printf("usage: file with player {.s}");
+		printf("Usage: <sourcefile.s> other extension not supported\n");
 		return (0);
 	}
 	if ((fd = open(argv[1], O_RDONLY)) == -1)
@@ -209,7 +243,7 @@ int main(int argc , char **argv)
 		printf("Cannot read \"{%s}\" file", argv[1]);
 		return 0;
 	}
-	if (!validate(fd, data))
+	if (!validate(fd, data, argv[1]))
 		return (0);
 	int i  = 0;
 	while (data->array[i])
